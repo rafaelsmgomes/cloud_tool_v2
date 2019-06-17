377d9be2f99debb6770f17ab1415e21dfd8acace
Mike Dacanay <migd64@gmail.com>
2019-06-17 14:46:44 -0700

    added release and amended change event on dial

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,16 @@ import 'jquery';
 import 'jquery-knob';
 import 'cpr_scrollpath';
 
-import * as f from './functions';
 import * as v from './variables';
+
 import {e} from './views/base';
+import * as dk from './views/dialView';
 
 $(document).ready(function(){
 
 	e.btnProgress.sp(v.movement);
 	$('.btn__progress--1').click();
-	f.dialKnob(e.dialTracker);
+	dk.dialKnob(e.dialTracker);
 
 });  
 
