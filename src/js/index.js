17b9cacb477aaac5a7d7edc339f7b5c564450372
Mike Dacanay <migd64@gmail.com>
2019-06-28 18:44:21 -0700

    finished animating lines

--- src/js/index.js
+++ src/js/index.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
 
 	state.pageNum = new Page();
 
-	// $('.btn__progress--6').click();
+	$('.btn__progress--5').click();
 
 	e.btnProgress.on('click',function(){
 		const value = $(this).data('val');
--- src/js/views/buttonProgressView.js
+++ src/js/views/buttonProgressView.js
@@ -1,4 +1,5 @@
 export const animateStuff = (btnVal) => {
+
 	if(btnVal === 1){
 		$('.landing__container').addClass('landing__container--fadeout');
 		$('.landing__title').addClass('landing__title--fadeout');
@@ -6,36 +7,56 @@ export const animateStuff = (btnVal) => {
 		
 		setTimeout(function(){
 			$('.page--2').addClass('activate');				
+			$('.line--1, .line--2').addClass('activate');
+			$('.btn__regress--2').addClass('activate');
 		},7500);		
-	}else if(btnVal === 2){
+	}
+		else if(btnVal === 2){
 		$('.page--2').removeClass('activate');
 
 		setTimeout(function(){
-			$('.page--3').addClass('activate');				
+			$('.page--3').addClass('activate');
+			$('.line--2-a, .line--3, .line--4').addClass('activate');
+			$('.btn__regress--3').addClass('activate');				
 		},7500);		
-	}else if(btnVal === 3){
+	}
+	else if(btnVal === 3){
 		$('.page--3').removeClass('activate');
 
 		setTimeout(function(){
-			$('.page--4').addClass('activate');				
+			$('.page--4').addClass('activate');		
+			$('.line--5, .line--6, .line--7').addClass('activate');
+			$('.btn__regress--4').addClass('activate');		
 		},9000);		
 	}else if(btnVal === 4){
 		$('.page--4').removeClass('activate');
 
 		setTimeout(function(){
-			$('.page--5').addClass('activate');				
+			$('.page--5').addClass('activate');
+			$('.line--8').addClass('activate');
+			$('.btn__regress--5').addClass('activate');				
 		},6000-1000);		
 	}else if(btnVal === 5){
 		$('.page--5').removeClass('activate');
 		
 		setTimeout(function(){
-			$('.page--6').addClass('activate');				
+			$('.page--6').addClass('activate');		
+			$('.line--9').addClass('activate');
+			$('.btn__regress--6').addClass('activate');						
 		},6000-1000);		
 	}else if(btnVal === 6){
 		$('.page--6').removeClass('activate');
+			
+		setTimeout(function(){
+			$('.page--7').addClass('activate');		
+			$('.line--10, .line--11').addClass('activate');
+			$('.btn__regress--7').addClass('activate');				
+		},7500);	
+	}else if(btnVal === 7){
+		$('.page--7').removeClass('activate');
 		
 		setTimeout(function(){
-			$('.page--7').addClass('activate');				
+			$('.page--8').addClass('activate');				
 		},7500);	
 	}
 
