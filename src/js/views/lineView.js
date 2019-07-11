05943893a7270ab5413beb73fdfd726bb1de649d
Rafael Gomes <rafaelsmgomes@MacBook-Air-de-Rafael.local>
2019-07-10 21:34:49 -0700

    Fixed line activation for DesktopS- Q1

--- src/js/views/lineView.js
+++ src/js/views/lineView.js
@@ -1,8 +1,10 @@
 export const resizeLines = width => {	
 	let size1 = 102.6;
+	let size2 = 92.7;
 
 	if(width <= 1440){
 		size1 = 81.15;
+		size2 = 106.7;
 	}
 
 	$('.line--1').attr({
@@ -10,7 +12,8 @@ export const resizeLines = width => {
 	});
 	$('.line--2').attr({
 		y1: `${size1}rem`,
-	  y2: `${size1}rem`
+	  y2: `${size1}rem`,
+	  x2: `${size2}rem`
 	});
 	$('.line--2-a').attr({
 		y1: `${size1}rem`,
