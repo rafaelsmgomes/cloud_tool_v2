760648fa6b35237fe9414ad3f83a0264cadb18a0
Rafael Gomes <rafaelsmgomes@MacBook-Air-de-Rafael.local>
2019-07-10 22:28:41 -0700

    Adjusted Line height and activation for Q3

--- src/js/views/lineView.js
+++ src/js/views/lineView.js
@@ -1,12 +1,17 @@
 export const resizeLines = width => {	
 	let size1 = 102.6;
 	let size2 = 92.7;
+	let size3 = 153.1;
+	let size4 = 278.6;
 
 	if(width <= 1440){
 		size1 = 81.15;
 		size2 = 106.7;
+		size3 = 131.7;
+		size4 = 292.6;
 	}
 
+	//Line for Question 1
 	$('.line--1').attr({
 	  y2: `${size1}rem`
 	});
@@ -22,4 +27,22 @@ export const resizeLines = width => {
 	$('.line--3').attr({
 	  y1: `${size1}rem`
 	});
+
+	//Line for Question 3
+	$('.line--3').attr({
+		y2: `${size3}rem`,
+	});
+	$('.line--4').attr({
+		y1: `${size3}rem`,
+		y2: `${size3}rem`,
+		x2: `${size4}rem`
+	});
+	$('.line--5').attr({
+		y1: `${size3}rem`,
+		y2: `${size3}rem`
+	});
+	$('.line--6').attr({
+		y1: `${size3}rem`
+	});
+
 }
