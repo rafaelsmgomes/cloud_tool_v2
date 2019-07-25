9e86a3e40cce40d9ea18381c5c361c9e34f8c1f1
Mike Dacanay <migd64@gmail.com>
2019-07-25 00:40:52 -0700

    animation on line resut page stopped after scroll

--- src/js/index.js
+++ src/js/index.js
@@ -34,7 +34,19 @@ import scrollJson from './scrollDown.json';
 
 $(document).ready(function(){
 
- 	const state = {};
+ 	const state = {};	
+/****************  CLOUD/ LOTTIE INIT  ********************/
+	var scrollTip = lottie.loadAnimation({
+	  container: document.getElementById('scroll_down'),
+	  renderer: 'svg',
+	  autoplay: true,
+	  animationData: scrollJson,
+	  loop: true,
+	});	
+
+	console.log(scrollTip.isPaused);
+
+
 
 /****** CTA POPUPS ******/ 
 	$('.cta__btn').on('click',function(){
@@ -60,8 +72,17 @@ $(document).ready(function(){
 	});
 
 	$('.page--8').on('scroll',function(){
+		const pos = $(this).scrollTop();
 		$('.page--8').addClass('activate');
-	})
+		
+		if(pos > 0 ){
+			$('.line--13').addClass('deactivate');
+			scrollTip.isPaused = true;		
+		}else{
+			$('.line--13').removeClass('deactivate');
+			scrollTip.isPaused = false;
+		}
+	});
 
 /****** CPRSCROLLPATH/MOVEMENT CONTROLLER ******/
  
@@ -346,14 +367,7 @@ $(document).ready(function(){
 		css.displayEloqua();
 	});
 
-/****************  CLOUD/ LOTTIE INIT  ********************/
-	lottie.loadAnimation({
-	  container: document.getElementById('scroll_down'),
-	  renderer: 'svg',
-	  autoplay: true,
-	  animationData: scrollJson,
-	  loop: true,
-	});
+
 });
 
 /****************  CLOUD LOTTIE  ********************/
