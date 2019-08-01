e3d79b65bca57f426ccac3c24684ff76b4ae1518
Mike Dacanay <migd64@gmail.com>
2019-08-01 00:12:39 -0700

    data session migrating complete

--- src/js/views/detailedResults.js
+++ src/js/views/detailedResults.js
@@ -1,3 +1,20 @@
 export const displayResults = (session) => {
-	console.log(session);
+	$('#result-ex--1').append(
+		`<span class='block'>${session.dial1}</span>` 
+	);
+	$('#result-ex--2').append(
+		`<span class='block'>${session.dial2}</span>` 
+	);
+	$('#result-ex--3').append(
+		`<span class='block'>${session.selector1}</span>` 
+	);
+	$('#result-ex--4').append(
+		`<span class='block'>${session.dial3}</span>` 
+	);
+	$('#result-ex--5').append(
+		`<span class='block'>${session.slider1}</span>` 
+	);
+	$('#result-ex--6').append(
+		`<span class='block'>${session.slider2}</span>` 
+	);
 };
