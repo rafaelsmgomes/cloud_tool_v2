c2fc5b445e1c84283572ddc4ecba29c37eda1fec
Mike Dacanay <migd64@gmail.com>
2019-08-08 18:22:06 -0700

    started logic on det slider

--- src/js/models/CompareResults.js
+++ src/js/models/CompareResults.js
@@ -14,7 +14,6 @@ export default class CompareResults {
     	this.val['1'] = (Number(user.dial1) - peer[1]);
     	this.val['2'] = (Number(user.dial2) - peer[2]);
     	this.val['3'] = user.selector1;    	
-    	// console.log(user)
     	this.val['4'] = (Number(user.dial3) - peer[4]);
     	// this.val['5'] = (Number(user.dial1) - peer[1]);
     	// this.val['6'] = (Number(user.dial1) - peer[1]);
