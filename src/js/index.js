07f0e3453efd6d3c5ffb2f8c695d92db584541cf
Rafael Gomes <rafaelsmgomes@MacBook-Air-de-Rafael.local>
2019-08-12 12:10:16 -0700

    Dev compiled files

--- src/js/index.js
+++ src/js/index.js
@@ -300,7 +300,7 @@ $(document).ready(function(){
 	});
 
 
-	// $('.btn__progress--3').click(); 
+	// $('.btn__progress--1').click(); 
 
 	e.btnBackX.on('click',function(){
 		const contextValue = e.btnBackX.attr('context');
