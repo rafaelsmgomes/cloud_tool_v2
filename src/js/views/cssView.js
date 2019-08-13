d3757f4e013f31a4de04c4f7147af5d5fe403d25
Rafael Gomes <rafaelsmgomes@MacBook-Air-de-Rafael.local>
2019-08-13 15:31:18 -0700

    Header Background-color to white on Results Page

--- src/js/views/cssView.js
+++ src/js/views/cssView.js
@@ -1,8 +1,10 @@
 export const changeBodyColor = (color) => {
   if(color === 'white'){
-  	$('body').addClass('bg-white');
+	  $('body').addClass('bg-white');
+	  $('.header__background').addClass('bg-white');
   }else{
-  	$('body').removeClass('bg-white');
+	  $('body').removeClass('bg-white');
+	  $('.header__background').removeClass('bg-white');
   }
 };
 
